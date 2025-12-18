25637ec159fcaf0877f151a7fa225b9b8411b4c6
iqbal hasan <iamekbal75@gmail.com>
2025-12-18 18:16:37 +0600

    add some

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
        <h1>home page here </h1>
+       <h1> akn e sokol page thakbe </h1>
     </div>
   );
 }
